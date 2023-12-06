6889bf7b01f70a5efa129a3085db4bace5178d6b
Marine CHAMOUX <marine.chamoux@PAR-4300-LA.aldebaran.lan>
2023-12-06 12:05:56 +0100

    Ajout d'un bouton pour télécharger le CV

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
+
+import './About.css'
 
 function About() {
   return (
     <div>
-      {/* Contenu de la page À Propos */}
-      <h1>À Propos</h1>
+        {/* Contenu de la page À Propos */}
+        <a href="/res/cv-marinechamoux-SWEngineeringManager.pdf" download className="cv-download-button">
+            <span className="button-text">Available for new challenges</span>
+            <span className="button-icon">
+                <FontAwesomeIcon icon={faDownload} /> {/* Remplacez faDownload par l'icône de votre choix */}
+            </span>
+        </a>
+        <h2>Software engineering manager & Product Owner in Robotics</h2>
+        <h1>Marine Chamoux</h1>
     </div>
   );
 }
