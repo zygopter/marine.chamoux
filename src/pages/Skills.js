30d511d6dd72010d0255e01374e9e77153b7c9da
Marine CHAMOUX <marine.chamoux@PAR-4300-LA.aldebaran.lan>
2023-12-20 11:28:12 +0100

    Improve skillbar UI and animation

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -4,25 +4,33 @@ import './Skills.css'
 
 
 function SkillBar({ skill, percentage }) {
-  const [width, setWidth] = useState('0%');
-
-  useEffect(() => {
-    // Déclenchez l'animation au chargement du composant
-    const handleAnimation = () => {
-      setWidth(`${percentage}%`);
-    };
-    handleAnimation();
-  }, [percentage]);
-
-  return (
-    <div className="skill">
-      <div className="skill-name">{skill}</div>
-      <div className="skill-bar">
-        <div className="skill-percentage" style={{ width }}></div>
+    const [width, setWidth] = useState('0%');
+  
+    useEffect(() => {
+      // Déclenchez l'animation au chargement du composant
+      const handleAnimation = () => {
+        setWidth(`100%`);
+      };
+      handleAnimation();
+    }, [percentage]);
+  
+    return (
+      <div className="skill">
+        <div className="skill-name">{skill}</div>
+        <div className="skill-bar">
+          <div 
+            className="skill-percentage" 
+            style={{ 
+                width,
+                clipPath: `inset(0 calc(${100 - percentage}%) 0 0)`
+            }}
+          ></div>
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
+  
+  
 
 function Skills() {
     return (
@@ -34,8 +42,14 @@ function Skills() {
         <h3 className='technical-skills'>Software development</h3>
         <div className='skills-bars'>
         <SkillBar skill="C++" percentage={70} />
-        <SkillBar skill="Kotlin" percentage={70} />
-        <SkillBar skill="Web" percentage={50} />
+        <SkillBar skill="Android, Kotlin" percentage={70} />
+        <SkillBar skill="React" percentage={50} />
+        <SkillBar skill="Human-Robot Interaction" percentage={70} />
+        <SkillBar skill="Robotics, RaaS" percentage={50} />
+        <SkillBar skill="IA, Perception, IA tooling" percentage={50} />
+        <SkillBar skill="Git, Github, Gitlab" percentage={70} />
+        <SkillBar skill="SAFe, Agile" percentage={50} />
+        <SkillBar skill="Jira, Confluence, Redmine" percentage={50} />
         </div>
       </div>
     );
